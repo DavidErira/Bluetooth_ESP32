d6a1bf05e0b62ce539bbc2df99b59a7a4fbd94fc
DavidErira <davidestebane1298@gmail.com>
2020-08-10 23:09:11 -0500

    firs commit

---
+++ BT_MPU.ino
@@ -0,0 +1,95 @@
+//#include <ESP8266WiFi.h>
+#include <BluetoothSerial.h>
+#include <Wire.h>
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+//variables de BT
+
+BluetoothSerial SerialBT;
+String msg="";
+
+//variables del MPU------------------------------------------------------
+
+#define MPU 0x68
+ 
+//Ratios de conversion
+#define A_R 16384.0 // 32768/2
+#define G_R 131.0 // 32768/250
+ 
+//Conversion de radianes a grados 180/PI
+#define RAD_A_DEG = 57.295779
+ 
+
+int16_t AcX, AcY, AcZ, GyX, GyY, GyZ;
+ 
+//Angulos
+float Acc[2];
+float Gy[3];
+float Angle0;
+
+
+long tiempo_prev;
+float dt;
+
+//variables del MPU----------------------------------------------------------
+
+
+
+void setup() {
+
+    Serial.begin(115200);
+
+    SerialBT.begin("ESP32test"); //Bluetooth device name
+     //setup MPU-------------------------------------------------------------
+    Wire.begin(4,5); // D2(GPIO4)=SDA / D1(GPIO5)=SCL
+    Wire.beginTransmission(MPU);
+    Wire.write(0x6B);
+    Wire.write(0);
+    Wire.endTransmission(true);
+
+}
+
+void loop() {
+
+    //Leer los valores del Acelerometro de la IMU
+   Wire.beginTransmission(MPU);
+   Wire.write(0x3B); //Pedir el registro 0x3B - corresponde al AcX
+   Wire.endTransmission(false);
+   Wire.requestFrom(MPU,6,true);   //A partir del 0x3B, se piden 6 registros
+   AcX=Wire.read()<<8|Wire.read(); //Cada valor ocupa 2 registros
+   AcY=Wire.read()<<8|Wire.read();
+   AcZ=Wire.read()<<8|Wire.read();
+ 
+   //A partir de los valores del acelerometro, se calculan los angulos Y, X
+   //respectivamente, con la formula de la tangente.
+   Acc[1] = atan(-1*(AcX/A_R)/sqrt(pow((AcY/A_R),2) + pow((AcZ/A_R),2)))*RAD_TO_DEG;
+   Acc[0] = atan((AcY/A_R)/sqrt(pow((AcX/A_R),2) + pow((AcZ/A_R),2)))*RAD_TO_DEG;
+ 
+   //Leer los valores del Giroscopio
+   Wire.beginTransmission(MPU);
+   Wire.write(0x43);
+   Wire.endTransmission(false);
+   Wire.requestFrom(MPU,6,true);   //A partir del 0x43, se piden 6 registros
+   GyX=Wire.read()<<8|Wire.read(); //Cada valor ocupa 2 registros
+   GyY=Wire.read()<<8|Wire.read();
+   GyZ=Wire.read()<<8|Wire.read();
+ 
+   //Calculo del angulo del Giroscopio
+   Gy[0] = GyX/G_R;
+   Gy[1] = GyY/G_R;
+   Gy[2] = GyZ/G_R;
+
+   dt = (millis() - tiempo_prev) / 1000.0;
+   tiempo_prev = millis();
+   //Aplicar el Filtro Complementario
+   Angle0 = 0.98 *(Angle0+Gy[0]*dt) + 0.02*Acc[0];
+
+   delay(100);
+
+   msg="#"+String(Angle0);
+   SerialBT.print(msg);
+
+}
